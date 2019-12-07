b1436fff3e0c017991f26a63bd4cd5b948b2509d
Hemanthjetti <58408432+Hemanthjetti@users.noreply.github.com>
2019-12-07 08:58:23 +0530

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int m,p,c,tot;
+	float avg;
+	printf("\n enter marks in maths,physics,chemistry");
+	scanf("%d%d%d",&m,&p,&c);
+    tot=m+p+c;
+    avg=(float)tot/3;
+    printf("\n total marks :%d",tot);
+    printf("\n Average : %f",avg);
+}
